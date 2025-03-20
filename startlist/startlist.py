93d2d5af711d048e13160c163806a3bd110797a1
Stuart Lynne <stuart.lynne@gmail.com>
2025-03-20 09:14:09 -0700

    bump version

--- startlist/startlist.py
+++ startlist/startlist.py
@@ -20,7 +20,7 @@ from startlist.genbibs import GenBibs
 from startlist.geninfo import GenInfo
 from startlist.genpdf import GenPDF
 
-__version__ = "0.4.5"
+__version__ = "0.5.0"
 
 
 def format_date(input_date):
